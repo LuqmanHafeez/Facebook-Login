a2d7701d64ba4c498b84745c2180bf9b1ec43982
LuqmanHafeez <luqmankhanktk19@gmail.com>
2022-09-02 11:19:12 +0500

    facebook login

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -46,7 +46,7 @@ class _LoginPageState extends State<LoginPage> {
               label: const Text("Logout"),
               onPressed: () {
                 Provider.of<LoginController>(context, listen: false)
-                    .facebookLogin();
+                    .facebookLogout();
               })
         ],
       ),
